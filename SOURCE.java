20d16ac9ba8ccd2425b81ce33e61b56ff07f2a1d
Loginme <94568aa@gmail.com>
2014-05-23 21:59:25 -0700

    Create SOURCE.java

---
+++ SOURCE.java
@@ -0,0 +1,16 @@
+
+public class HiWorld {
+	
+	//This program writes Hello World! and
+	//Bye World!
+
+	public static void main(String[] args) {
+
+System.out.println("BootsAndCatsAndBootsAndCatsAndBotsAndCats");
+
+System.out.println("Bye World!");
+	//end here
+
+	}
+
+}
